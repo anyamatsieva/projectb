88116fdf031133a2b649dc3e116d6718d2b2dbc8
anyamatsieva <119114354+anyamatsieva@users.noreply.github.com>
2024-12-26 00:27:53 +0500

    Add files via upload

---
+++ front/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import AddWork from "../components/AddWork";
+import CheckWork from "../components/CheckWork";
+import TransferRights from "../components/TransferRights";
+
+const Home = () => {
+  return (
+    <div>
+      <AddWork />
+      <CheckWork />
+      <TransferRights />
+    </div>
+  );
+};
+
+export default Home;
